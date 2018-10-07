623c89c3b0abe47d0e90494b5601f633252ff3ca
eMIKARp <emil.karpowicz@gmail.com>
2018-10-07 15:19:29 +0200

    Another commit

--- WebContent/scripts.js
+++ WebContent/scripts.js
@@ -3,6 +3,8 @@
  */
 
 var number=Math.floor(Math.random()*5+1);
+var timer1 = 0;
+var timer2 = 0;
 
 function changeSlide(){
 	number++;
@@ -10,10 +12,20 @@ function changeSlide(){
 	var file = "<img src=\""+number+".JPG\"/>";
 	document.getElementById("slider").innerHTML = file;
 	$("#slider").fadeIn(500);
-	setTimeout(() => {
+	timer1 = setTimeout(() => {
 		changeSlide();
 	}, 5000);
-	setTimeout(() => {
+	timer2 = setTimeout(() => {
 		$("#slider").fadeOut(500);
 	}, 4500);
+}
+
+function setSlide(slideNumber){
+	clearTimeout(timer1);
+	clearTimeout(timer2);
+	number = slideNumber-1;
+	$("#slider").fadeOut(500);
+	setTimeout(() => {
+		changeSlide();
+	}, 500);
 }
